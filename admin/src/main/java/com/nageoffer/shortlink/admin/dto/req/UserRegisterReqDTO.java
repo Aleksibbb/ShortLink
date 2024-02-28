d1bd911fb2f03a2edce45494f8d47ba6b5f5b0ff
Aleksibbb <wyx200078@163.com>
2024-02-28 21:31:51 +0800

    feature: 开发后管系统用户注册相关功能

--- admin/src/main/java/com/nageoffer/shortlink/admin/dto/req/UserRegisterReqDTO.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/dto/req/UserRegisterReqDTO.java
@@ -3,19 +3,19 @@ package com.nageoffer.shortlink.admin.dto.req;
 import lombok.Data;
 
 /**
- *
+ * 用户注册请求参数
  */
 @Data
-public class UserReqDTO {
-    /**
-     * id
-     */
-    private Long id;
-
+public class UserRegisterReqDTO {
     /**
      * 用户名
      */
     private String username;
+
+    /**
+     * 密码
+     */
+    private String password;
 
     /**
      * 真实姓名
--- admin/src/main/java/com/nageoffer/shortlink/admin/service/impl/UserServiceImpl.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/service/impl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.nageoffer.shortlink.admin.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -7,12 +8,15 @@ import com.nageoffer.shortlink.admin.common.convention.exception.ClientException
 import com.nageoffer.shortlink.admin.common.enums.UserErrorCodeEnum;
 import com.nageoffer.shortlink.admin.dao.entity.UserDO;
 import com.nageoffer.shortlink.admin.dao.mapper.UserMapper;
+import com.nageoffer.shortlink.admin.dto.req.UserRegisterReqDTO;
 import com.nageoffer.shortlink.admin.dto.resp.UserRespDTO;
 import com.nageoffer.shortlink.admin.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.redisson.api.RBloomFilter;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
+
+import static com.nageoffer.shortlink.admin.common.enums.UserErrorCodeEnum.*;
 
 /**
  * 用户接口实现层
@@ -40,11 +44,28 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements 
 
     /**
      * 查询用户名是否存在
-     * @param username
-     * @return true：用户不存在；false：用户存在
      */
     @Override
     public boolean hasUsername(String username) {
         return userRegisterCachePenetrationBloomFilter.contains(username);
+    }
+
+    /**
+     * 用户注册
+     * @param requestParam：用户请求参数
+     */
+    @Override
+    public void register(UserRegisterReqDTO requestParam) {
+        // 1. 判断用户名是否存在
+        if(hasUsername(requestParam.getUsername())){
+            throw new ClientException(USER_NAME_EXIST);
+        }
+        int inserted = baseMapper.insert(BeanUtil.toBean(requestParam, UserDO.class));
+        // 2. 判断用户记录是否创建成功
+        if(inserted < 1){
+            throw new ClientException(USER_SAVE_ERROR);
+        }
+        // 3. 将用户名信息保存到布隆过滤器
+        userRegisterCachePenetrationBloomFilter.add(requestParam.getUsername());
     }
 }
