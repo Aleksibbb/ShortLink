54f90410fe1f6bf176b94c8925a7a15767531d92
Aleksib <Aleksib@2000.com>
2024-02-28 11:17:33 +0800

    feature:开发 “返回用户脱敏信息”接口

--- admin/src/main/java/com/nageoffer/shortlink/admin/controller/UserController.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.nageoffer.shortlink.admin.controller;
 
+import cn.hutool.core.bean.BeanUtil;
 import com.nageoffer.shortlink.admin.common.convention.result.Result;
 import com.nageoffer.shortlink.admin.common.convention.result.Results;
+import com.nageoffer.shortlink.admin.dto.resp.UserActualRespDTO;
 import com.nageoffer.shortlink.admin.dto.resp.UserRespDTO;
 import com.nageoffer.shortlink.admin.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -19,10 +21,18 @@ public class UserController {
     private final UserService userService;  // 通过构造器方式注入 配合@RequiredArgsConstructor
 
     /**
-     * 根据用户名查询用户信息
+     * 根据用户名查询用户信息（已脱敏）
      */
     @GetMapping("/api/short-link/admin/v1/user/{username}")
     public Result<UserRespDTO> getUserByUsername(@PathVariable("username") String username){
         return Results.success(userService.getUserByUsername(username));    // 在 接口实现层已经实现异常拦截
+    }
+
+    /**
+     * 根据用户名查询用户信息（无脱敏）
+     */
+    @GetMapping("/api/short-link/admin/v1/actual/user/{username}")
+    public Result<UserActualRespDTO> getActualUserByUsername(@PathVariable("username") String username){
+        return Results.success(BeanUtil.toBean(userService.getUserByUsername(username), UserActualRespDTO.class));    // 在 接口实现层已经实现异常拦截
     }
 }
