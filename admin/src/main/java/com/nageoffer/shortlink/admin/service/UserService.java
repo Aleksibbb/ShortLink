77d4883f16921889b3b42c4fd9b144ce49f5e5e3
Aleksibbb <wyx200078@163.com>
2024-02-29 18:18:09 +0800

    feature: 后管系统——用户修改功能

--- admin/src/main/java/com/nageoffer/shortlink/admin/service/UserService.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/service/UserService.java
@@ -3,6 +3,7 @@ package com.nageoffer.shortlink.admin.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.nageoffer.shortlink.admin.dao.entity.UserDO;
 import com.nageoffer.shortlink.admin.dto.req.UserRegisterReqDTO;
+import com.nageoffer.shortlink.admin.dto.req.UserUpdateReqDTO;
 import com.nageoffer.shortlink.admin.dto.resp.UserRespDTO;
 
 /**
@@ -15,4 +16,6 @@ public interface UserService extends IService<UserDO> {
     boolean hasUsername(String username);
 
     void register(UserRegisterReqDTO requestParam);
+
+    void update(UserUpdateReqDTO requestParam);
 }
