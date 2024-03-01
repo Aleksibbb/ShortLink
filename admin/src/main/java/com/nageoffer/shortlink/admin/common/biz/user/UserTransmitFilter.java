b54d282eecd06b283ad657929bedea2c30b7810b
Aleksibbb <wyx200078@163.com>
2024-03-01 11:56:29 +0800

    feature: 开发短链接后管系统——用户上下文封装

---
+++ admin/src/main/java/com/nageoffer/shortlink/admin/common/biz/user/UserTransmitFilter.java
@@ -0,0 +1,38 @@
+package com.nageoffer.shortlink.admin.common.biz.user;
+
+import com.alibaba.fastjson2.JSON;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+import java.io.IOException;
+
+import static com.nageoffer.shortlink.admin.common.constant.RedisCacheConstant.USER_LOGIN_KEY;
+
+/**
+ * 用户信息传输过滤器
+ */
+
+@RequiredArgsConstructor
+public class UserTransmitFilter implements Filter {
+    private final StringRedisTemplate stringRedisTemplate;
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        // 1. 从请求头中获取 用户名、token
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        String username = httpServletRequest.getHeader("username");
+        String token = httpServletRequest.getHeader("token");
+        // 2. 将用户信息保存到 UserContext
+        Object userInfoJsonStr = stringRedisTemplate.opsForHash().get(USER_LOGIN_KEY + username, token);
+        if (userInfoJsonStr != null) {
+            UserInfoDTO userInfoDTO = JSON.parseObject(userInfoJsonStr.toString(), UserInfoDTO.class);
+            UserContext.setUser(userInfoDTO);
+        }
+        try {
+            filterChain.doFilter(servletRequest, servletResponse);
+        } finally {
+            UserContext.removeUser();
+        }
+    }
+}
