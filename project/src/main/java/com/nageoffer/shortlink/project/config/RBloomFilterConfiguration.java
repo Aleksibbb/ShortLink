023d523b5d7e7623fec7b26f3bc1eee5bf8abaf2
Aleksibbb <wyx200078@163.com>
2024-03-01 23:50:41 +0800

    feature: 短链接中台系统——短链接管理初始化+新增短链接

---
+++ project/src/main/java/com/nageoffer/shortlink/project/config/RBloomFilterConfiguration.java
@@ -0,0 +1,23 @@
+package com.nageoffer.shortlink.project.config;
+
+import org.redisson.api.RBloomFilter;
+import org.redisson.api.RedissonClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 布隆过滤器配置
+ */
+@Configuration
+public class RBloomFilterConfiguration {
+
+    /**
+     * 防止创建短链接查询数据库（ full_short_url 是否存在）的布隆过滤器
+     */
+    @Bean
+    public RBloomFilter<String> shortUriCreateCachePenetrationBloomFilter(RedissonClient redissonClient) {
+        RBloomFilter<String> cachePenetrationBloomFilter = redissonClient.getBloomFilter("short-link:porject:shortUriCreate:BloomFilter");
+        cachePenetrationBloomFilter.tryInit(100000000L, 0.001); // 用户量：一亿   误判率：0.1%
+        return cachePenetrationBloomFilter;
+    }
+}
---
+++ project/src/main/java/com/nageoffer/shortlink/project/service/impl/ShortLinkServiceImpl.java
@@ -0,0 +1,87 @@
+package com.nageoffer.shortlink.project.service.impl;
+
+import cn.hutool.core.bean.BeanUtil;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.nageoffer.shortlink.admin.common.convention.exception.ServiceException;
+import com.nageoffer.shortlink.project.dao.entity.ShortLinkDO;
+import com.nageoffer.shortlink.project.dao.mapper.ShortLinkMapper;
+import com.nageoffer.shortlink.project.dto.req.ShortLinkCreateReqDTO;
+import com.nageoffer.shortlink.project.dto.resp.ShortLinkCreateRespDTO;
+import com.nageoffer.shortlink.project.service.ShortLinkService;
+import com.nageoffer.shortlink.project.toolkit.HashUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RBloomFilter;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.stereotype.Service;
+
+/**
+ * 短链接接口实现层
+ */
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLinkDO> implements ShortLinkService {
+
+    private final RBloomFilter<String> shortUriCreateCachePenetrationBloomFilter;
+    @Override
+    public ShortLinkCreateRespDTO createShortLink(ShortLinkCreateReqDTO requestParam) {
+        // fullShortUrl ： 域名 + "/" + 短链接六位后缀
+        // 1. 获取短链接后缀
+        String shortLinkSuffix = generateSuffix(requestParam);
+        String fullShortUrl = requestParam.getDomain() + "/" + shortLinkSuffix;
+        // 2. 获取短链接实体对象（需要单独设置的属性：短链接、完整短链接、启用标识）
+        ShortLinkDO shortLinkDO = BeanUtil.toBean(requestParam, ShortLinkDO.class);
+        shortLinkDO.setShortUri(shortLinkSuffix);
+        shortLinkDO.setFullShortUrl(fullShortUrl);
+        shortLinkDO.setEnableStatus(0);     // 设置启用
+        try{
+            // 3. 保存到数据库
+            baseMapper.insert(shortLinkDO);
+        } catch (DuplicateKeyException ex) {    // 捕获到唯一索引冲突
+            // TODO 已经误判的短链接如何处理
+            // 短链接确实真实存在缓存
+            // 短链接不一定存在缓存中
+            LambdaQueryWrapper<ShortLinkDO> queryWrapper = Wrappers.lambdaQuery(ShortLinkDO.class)
+                    .eq(ShortLinkDO::getFullShortUrl, fullShortUrl);
+            ShortLinkDO hasShortLinkDO = baseMapper.selectOne(queryWrapper);
+            if(hasShortLinkDO != null){
+                // 数据库中确实存在
+                log.warn("短链接: {} 重复入库", fullShortUrl);
+                throw new ServiceException("短链接重复生成");
+            }
+        }
+        shortUriCreateCachePenetrationBloomFilter.add(fullShortUrl);
+        // 4. 返回响应对象
+        return ShortLinkCreateRespDTO.builder()
+                .gid(requestParam.getGid())
+                .fullShortUrl(shortLinkDO.getFullShortUrl())
+                .originUrl(requestParam.getOriginUrl())
+                .build();
+    }
+
+    /**
+     * 生成短链接六位后缀
+     */
+    private String generateSuffix(ShortLinkCreateReqDTO requestParam){
+        // 避免生成的短链接重复
+        int customGenerateCount = 0;
+        String shortUri;
+        while(true){
+            if(customGenerateCount > 10){
+                throw new ServiceException("短链接频繁生成，请稍后再试");
+            }
+            String originUrl = requestParam.getOriginUrl();
+            // originUrl += System.currentTimeMillis();
+            shortUri = HashUtil.hashToBase62(originUrl);
+            // 布隆过滤器中不存在
+            if(!shortUriCreateCachePenetrationBloomFilter.contains(requestParam.getDomain() + "/" + shortUri)){
+                break;
+            }
+            customGenerateCount++;
+        }
+        return shortUri;
+    }
+}
