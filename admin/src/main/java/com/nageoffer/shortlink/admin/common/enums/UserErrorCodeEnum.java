e2d919df3ff079696c452845b23be8bb7a2997d4
Aleksibbb <wyx200078@163.com>
2024-03-02 16:37:33 +0800

    feature: 短链接后管系统——开发用户登录验证拦截器返回友好提示信息

--- admin/src/main/java/com/nageoffer/shortlink/admin/common/enums/UserErrorCodeEnum.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/common/enums/UserErrorCodeEnum.java
@@ -6,6 +6,7 @@ import com.nageoffer.shortlink.admin.common.convention.errorcode.IErrorCode;
  * 服务端异常
  */
 public enum UserErrorCodeEnum implements IErrorCode {
+    USER_TOKEN_FAIL("A000200", "用户Token验证失败"),
 
     USER_NULL("B000200", "用户记录不存在"),
 
