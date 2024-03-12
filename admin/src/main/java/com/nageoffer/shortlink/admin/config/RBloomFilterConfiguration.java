1191a5318d6c9f0532cbc4016aa16e07cfccbc5b
Aleksibbb <wyx200078@163.com>
2024-03-12 14:49:29 +0800

    refactor：修改用户注册布隆过滤器名称

--- admin/src/main/java/com/nageoffer/shortlink/admin/config/RBloomFilterConfiguration.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/config/RBloomFilterConfiguration.java
@@ -5,7 +5,8 @@ import org.redisson.api.RedissonClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import static com.nageoffer.shortlink.admin.common.constant.RedisCacheConstant.BLOOMFILTER;
+import static com.nageoffer.shortlink.admin.common.constant.RedisCacheConstant.BLOOM_FILTER;
+
 
 /**
  * 布隆过滤器配置
@@ -18,7 +19,7 @@ public class RBloomFilterConfiguration {
      */
     @Bean
     public RBloomFilter<String> userRegisterCachePenetrationBloomFilter(RedissonClient redissonClient) {
-        RBloomFilter<String> cachePenetrationBloomFilter = redissonClient.getBloomFilter(BLOOMFILTER);
+        RBloomFilter<String> cachePenetrationBloomFilter = redissonClient.getBloomFilter(BLOOM_FILTER);
         cachePenetrationBloomFilter.tryInit(100000000L, 0.001); // 用户量：一亿   误判率：0.1%
         return cachePenetrationBloomFilter;
     }
